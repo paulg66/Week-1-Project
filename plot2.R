1eb1aec6ee43fe26b3c60b7721b5aee5aeac42b6
paulg66 <P.Gallagher66@gmail.com>
2017-03-31 22:21:58 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,10 @@
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dfSub <- df[(df$Date=="1/2/2007") | (df$Date=="2/2/2007"),]
+dfSub$Date <- as.Date(dfSub$Date, format="%d/%m/%Y")
+dfSub$Time <- strptime(dfSub$Time, format="%H:%M:%S")
+dfSub[1:1440,"Time"] <- format(dfSub[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+dfSub[1441:2880,"Time"] <- format(dfSub[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+dfSub$Global_active_power <- as.numeric(as.character(dfSub$Global_active_power))
+plot(dfSub$Time,dfSub$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
