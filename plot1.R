8035998a074a2009a0cadbb85cfb73540a7ca260
paulg66 <P.Gallagher66@gmail.com>
2017-03-31 21:53:25 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,5 @@
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+globalActivePower <- as.numeric(df$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col = "red", main ="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
